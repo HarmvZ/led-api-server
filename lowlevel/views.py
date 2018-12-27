9b94619e4fcfef44620cb21618950fd0cd0803c5
Harm van Zeeland <harm.van.zeeland@gmail.com>
2018-12-27 16:50:10 +0100

    Fix csrf decorator

--- lowlevel/views.py
+++ lowlevel/views.py
@@ -1,5 +1,6 @@
 import threading
 from django.views import generic, View
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from .models import Alarm
 from .led_libs import clock
@@ -14,8 +15,8 @@ class IndexView(generic.ListView):
         return Alarm.objects.all()
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class ClockView(View):
-    @csrf_exempt
     def post(self, request, *args, **kwargs):
         # show clock on leds, run never ending function in thread
         clock_thread = threading.Thread(target=clock.start_clock())
