05e8815eaf881c6dcae861011ec382d8b2abf551
Harm van Zeeland <harm.van.zeeland@gmail.com>
2018-12-27 16:37:02 +0100

    Ignore csrf for clock post view

--- lowlevel/views.py
+++ lowlevel/views.py
@@ -1,7 +1,9 @@
 import threading
 from django.views import generic, View
+from django.views.decorators.csrf import csrf_exempt
 from .models import Alarm
 from .led_libs import clock
+
 
 
 class IndexView(generic.ListView):
@@ -13,6 +15,7 @@ class IndexView(generic.ListView):
         return Alarm.objects.all()
 
 
+@csrf_exempt
 class ClockView(View):
     def post(self, request, *args, **kwargs):
         # show clock on leds, run never ending function in thread
