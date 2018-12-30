f7c5c63489d7cd2e65d4052404373436b9217794
Harm van Zeeland <harm.van.zeeland@gmail.com>
2018-12-30 17:54:09 +0100

    Add update and delete alarm views

--- lowlevel/urls.py
+++ lowlevel/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('alarms/create/', views.AlarmCreateView.as_view(), name='alarm-create'),
-    path('alarms/update/', views.AlarmUpdateView.as_view(), name='alarm-update'),
-    path('alarms/delete/', views.AlarmDeleteView.as_view(), name='alarm-delete'),
+    path('alarms/update/<int:pk>/', views.AlarmUpdateView.as_view(), name='alarm-update'),
+    path('alarms/delete/<int:pk>/', views.AlarmDeleteView.as_view(), name='alarm-delete'),
 
     # API functions
     path('show_color', views.ColorView.as_view(), name='color'),
