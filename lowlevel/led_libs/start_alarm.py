7c5ecc268d7ca88663dd2955d884247d2e9f8591
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-01-16 00:52:56 +0100

    Remove exponential wakeup temporarily

--- lowlevel/led_libs/start_alarm.py
+++ lowlevel/led_libs/start_alarm.py
@@ -58,7 +58,7 @@ class LedControl:
             log_range = np.exp(lin_range)
             # create exponential range from 0 to 1
             log_range = (log_range - 1) / (np.exp(100) - 1)
-            color = start_color + color_delta * log_range
+            color = start_color + color_delta * lin_range / 100  # log_range
             self.color_wipe(Color(int(color[0]), int(color[1]), int(color[2])))
             time.sleep(timestep / 1000)
 
