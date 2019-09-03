299e0e7480ade21529aebecf33bab89fc30c3e16
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-09-03 21:59:51 +0200

    Call correct animation

--- lowlevel/led_libs/animations.py
+++ lowlevel/led_libs/animations.py
@@ -111,4 +111,4 @@ if __name__ == "__main__":
         help="Choose animation ({})".format(fn_list_text),
     )
     args = parser.parse_args()
-    args.animation()
+    animationFuncs[args.animation()]
