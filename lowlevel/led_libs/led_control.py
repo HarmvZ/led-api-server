6143a5bc68a01c6278647693dd9f9daf36c62c8b
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-09-16 21:54:02 +0200

    Fix docker dev setup

--- lowlevel/led_libs/led_control.py
+++ lowlevel/led_libs/led_control.py
@@ -6,7 +6,8 @@ from . import settings
 from pathlib import Path
 import psutil
 from leds.settings import CLOCK_START_COMMAND
-
+from lowlevel.led_libs.utils.stoppable_thread import StoppableThread
+from lowlevel.led_libs.utils.strip_actions import StripActions
 
 def kill(proc_pid):
     """
@@ -19,24 +20,6 @@ def kill(proc_pid):
     for proc in process.children(recursive=True):
         proc.kill()
     process.kill()
-
-
-def bit24_to_3_bit8(val):
-    """
-    Convert 24-bit value to a 3 component 8 bit value
-    :param val: 24-bit value
-    :return: list of 3 8-bit int components
-    """
-    # Get binary value without 0b prefix
-    bin_val = bin(val)[2:]
-    # Get binary value padded with zeros
-    bin_val = "0" * (24 - len(bin_val)) + bin_val
-    # Extract components as int
-    red = int(bin_val[:8], 2)
-    green = int(bin_val[8:16], 2)
-    blue = int(bin_val[16:24], 2)
-    return red, green, blue
-
 
 # Singleton class as defined in:
 # https://python-3-patterns-idioms-test.readthedocs.io/en/latest/Singleton.html
@@ -56,76 +39,14 @@ class LedControl:
             # Intialize the library (must be called once before other functions).
             self.strip.begin()
 
-            self.process = None
+            self.thread = None
+            self.strip_actions = StripActions()
 
-        def color_wipe(self, color):
-            """Wipe color across display a pixel at a time."""
-            for i in range(self.strip.numPixels()):
-                self.strip.setPixelColor(i, color)
-            self.strip.show()
-
-        def wipe_clear(self):
-            self.color_wipe(Color(0, 0, 0))
-
-        def fill(self, r, g, b):
-            self.stop_process()
-            self.color_wipe(Color(r, g, b))
-
-        def fill_colors(self, color_matrix):
-            """Wipe color across display a pixel at a time."""
-            for i in range(self.strip.numPixels()):
-                color = Color(
-                    int(color_matrix[i][0]),
-                    int(color_matrix[i][1]),
-                    int(color_matrix[i][2]),
-                )
-                self.strip.setPixelColor(i, color)
-            self.strip.show()
-
-        def transition_to_color(self, r, g, b, steps=100, timestep=20):
-            """
-            Transition all leds to a color
-            :param r: red value 8-bit int
-            :param g: green value 8-bit int
-            :param b: blue value 8-bit int
-            :param steps: number of steps in transition
-            :param timestep: time that one step takes in ms
-            """
-            num_leds = self.strip.numPixels()
-            # get current colors and calculate difference with new color
-            current_colors = np.zeros((num_leds, 3))
-            color_deltas = np.zeros((num_leds, 3))
-            for i in range(num_leds):
-                current_colors[i] = bit24_to_3_bit8(self.strip.getPixelColor(i))
-                color_deltas[i] = current_colors[i] - np.array([r, g, b])
-
-            self.stop_process()
-            
-            for i in range(steps):
-                new_colors = (current_colors - color_deltas / (steps - 1) * i).astype(
-                    int
-                )
-                self.fill_colors(new_colors)
-                time.sleep(timestep / 1000)
-
-        def start_clock(self, bg=None, fg=None):
-            color_options = ""
-            if fg:
-                color_options += " -fg {}".format(fg)
-            if bg:
-                color_options += " -bg {}".format(bg)
-            self.start_process(CLOCK_START_COMMAND + color_options)
-
-        def start_process(self, command):
-            self.stop_process()
-            self.process = Popen(
-                command, stderr=None, stdin=None, stdout=None, shell=True
-            )
-
-        def stop_process(self):
-            if self.process is not None:
-                kill(self.process.pid)
-            self.wipe_clear()     
+        def strip_action(self, name, *args, **kwargs):
+            if issubclass(self.thread, StoppableThread) and self.thread.is_alive() and not self.thread.stopped():
+                self.thread.stop()
+                self.thread.join()
+            self.thread = getattr(self.strip_actions, name)(self.strip, *args, **kwargs)
 
     instance = None
 
--- lowlevel/led_libs/show_clock.py
+++ lowlevel/led_libs/show_clock.py
@@ -37,46 +37,7 @@ class LedControl:
         # Intialize the library (must be called once before other functions).
         self.strip.begin()
 
-    def write_matrix_to_strip(self, matrix):
-        height, width = matrix.shape
-        for h in range(height):
-            for w in range(width):
-                even = h % 2 == 0
-                if even:
-                    pixel_id = h * MATRIX_WIDTH + w
-                else:
-                    pixel_id = (h + 1) * MATRIX_WIDTH - w - 1
-                if matrix[h, w] == 1:
-                    self.strip.setPixelColor(pixel_id, self.fg)
-                else:
-                    self.strip.setPixelColor(pixel_id, self.bg)
-        self.strip.show()
 
-    @staticmethod
-    def create_binary_time_matrix():
-        now = datetime.datetime.now()
-        characters = now.strftime("%H%M%S")
-        matrix = np.zeros((MATRIX_HEIGHT, MATRIX_WIDTH))
-        indices = ((2, 4), (6, 8), (12, 14), (16, 18), (22, 24), (26, 28))
-        for h in range(2, 7):
-            for w in range(MATRIX_WIDTH):
-                for i, (start, end) in enumerate(reversed(indices)):
-                    if start <= w <= end:
-                        # Place character
-                        current_char = characters[i]
-                        current_char_matrix = NUMBERS[int(current_char)]
-                        matrix[h, w] = current_char_matrix[h - 2][w - start]
-                    if (w == 10 or w == 20) and (h == 3 or h == 5):
-                        # Place delimiter
-                        # matrix[h, w] = 1
-                        pass
-
-        return matrix
-
-    def set_time(self):
-        """Set time to current time on led pixels"""
-        matrix = self.create_binary_time_matrix()
-        self.write_matrix_to_strip(matrix)
 
 
 if __name__ == "__main__":
