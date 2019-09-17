105f3bebb79279ba61646cd2a688065e8bd91b66
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-09-17 16:55:44 +0200

    Add api urls

--- leds/urls.py
+++ leds/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("", include("lowlevel.urls")), path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("", include("lowlevel.urls")),
+    path("api/", include("api.urls")),
+    path("admin/", admin.site.urls)
+]
