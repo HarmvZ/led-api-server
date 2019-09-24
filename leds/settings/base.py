7823e9ffb05698cf985d805cf827dadbe05ba265
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-09-24 19:20:33 +0200

    Split up settings files

--- leds/settings/base.py
+++ leds/settings/base.py
@@ -19,14 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "2w@jrr9)6ps^#96r=$bn(1i=zi2f4xe%z%k=&h$nb)!$m-cua$"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
---
+++ leds/settings/local.py
@@ -0,0 +1,10 @@
+from leds.settings.base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "2w@jrr9)6ps^#96r=$bn(1i=zi2f4xe%z%k=&h$nb)!$m-cua$"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["localhost"]
