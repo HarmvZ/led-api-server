c7ab495410f0829db02a21a555cc601650f1815d
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-10-03 23:02:30 +0200

    Remove lazy pirate scheme

--- app/api/zmq_client.py
+++ app/api/zmq_client.py
@@ -1,53 +1,19 @@
 import zmq
 
-from django.conf import settings
 # Singleton class as defined in:
 # https://python-3-patterns-idioms-test.readthedocs.io/en/latest/Singleton.html
 class ZMQClient:
     class __ZMQClient:
         def __init__(self):
-            self.context = zmq.Context()
-            self.client = self.context.socket(zmq.REQ)
-            self.client.connect(settings.SERVER_ENDPOINT)
-            self.poll = zmq.Poller()
-            self.poll.register(self.client, zmq.POLLIN)
+            context = zmq.Context()
+            self.socket = context.socket(zmq.REQ)
+            self.socket.connect("tcp://zmq:5566")
 
         def perform_request(self, name, **kwargs):
-            message = {'action': name, 'kwargs': kwargs}
-
-            sequence = 0
-            retries_left = settings.REQUEST_RETRIES
-            while retries_left:
-                sequence += 1
-                self.client.send_pyobj(message)
-
-                expect_reply = True
-                while expect_reply:
-                    socks = dict(self.poll.poll(settings.REQUEST_TIMEOUT))
-                    if socks.get(self.client) == zmq.POLLIN:
-                        reply = self.client.recv_pyobj()
-                        if not reply:
-                            break
-                        else:
-                            retries_left = settings.REQUEST_RETRIES
-                            expect_reply = False
-                    else:
-                        # Socket is confused. Close and remove it.
-                        self.client.setsockopt(zmq.LINGER, 0)
-                        self.client.close()
-                        self.poll.unregister(self.client)
-                        retries_left -= 1
-                        if retries_left == 0:
-                            print("E: Server seems to be offline, abandoning")
-                            break
-                        print("I: Reconnecting and resending (%s)" % message)
-                        # Create new connection
-                        self.client = self.context.socket(zmq.REQ)
-                        self.client.connect(settings.SERVER_ENDPOINT)
-                        self.poll.register(self.client, zmq.POLLIN)
-                        self.client.send_pyobj(message)
-                
-            return reply
+            message = {"action": name, "kwargs": kwargs}
+            self.socket.send_pyobj(message)
+            result = self.socket.recv_pyobj()
+            return result
 
     instance = None
 
